224cc2c6c1096da321f8863cc7a6550876cee3fb
nanirama <nani.rama1982@gmail.com>
2022-01-09 08:18:10 +0530

    Mobile Nav changes

--- src/components/nav.js
+++ src/components/nav.js
@@ -25,20 +25,20 @@ const Nav = ({ isOpen, isSticky }) => {
   )
 
   const navClass = classNames({
-    "absolute z-998 top-0 w-full h-screen pt-14 pb-16 bg-white/95 overflow-y-scroll transition-right duration-300 in-expo shadow-xl": true,
-    "lg:flex-auto lg:items-center lg:ml-16 2xl:ml-20 lg:flex lg:relative lg:left-auto lg:top-auto lg:w-auto lg:h-auto lg:pt-0 lg:pb-0 lg:bg-none lg:bg-transparent lg:overflow-y-visible lg:shadow-none": true,
+    "absolute z-998 -right-full top-0 w-full h-screen pt-14 pb-16 bg-white/95 overflow-y-scroll transition-right duration-300 in-expo shadow-xl": true,
+    "lg:flex-1 lg:items-center lg:ml-16 2xl:ml-20 lg:flex lg:relative lg:right-auto lg:top-auto lg:w-auto lg:h-auto lg:pt-0 lg:pb-0 lg:bg-none lg:bg-transparent lg:overflow-y-visible lg:shadow-none": true,
     "!right-0": isOpen,
   })
 
   return (
     <nav className={navClass}>
-      <ul className="px-4 text-lg mt-6 space-y-4 lg:flex lg:h-auto lg:mr-auto lg:mt-0 lg:space-x-9 lg:space-y-0 lg:text-base">
+      <ul className="px-4 text-lg mt-6 space-y-4 lg:flex lg:h-auto lg:ml-auto lg:mt-0 lg:space-x-9 lg:space-y-0 lg:text-base">
         {site.siteMetadata.menuLinks.map((link, index) => (
-          <NavItem key={index} title={link?.title} url={link?.url} isSticky={isSticky} />
+          <NavItem key={index} title={link?.title} url={link?.url} />
         ))}
       </ul>
-
-      <div className="fixed bottom-12 w-full mt-6 ml-auto text-center px-4 space-y-4 lg:static lg:w-auto lg:mt-0 lg:ml-5 lg:px-0 lg:space-y-0 lg:space-x-4">
+      <div className="text-center">
+      <div className="flex lg:flex-row flex-col bottom-12 inline-flex mt-6 ml-auto text-center px-4 space-y-4 lg:static lg:w-auto lg:mt-0 lg:ml-5 lg:px-0 lg:space-y-0 lg:space-x-4">
         <Button
           to={site?.siteMetadata?.login}
           className={isSticky ? "btn btn-coral login btn btn-coral btn-lg text-lg lg:btn-outlined-green-dark lg:text-white lg:btn-sm border-0" : "login btn btn-coral btn-lg text-lg lg:btn-outlined-green-dark lg:text-white lg:btn-sm border-0"}>
@@ -50,6 +50,14 @@ const Nav = ({ isOpen, isSticky }) => {
           Get Started
         </Button>
       </div>
+      </div>
+      {/* <div className="fixed bottom-12 w-full mt-6 ml-auto text-center px-4 space-y-4 lg:static lg:w-auto lg:mt-0 lg:ml-5 lg:px-0 lg:space-y-0 lg:space-x-4">
+        <Button
+          to={site?.siteMetadata?.login}
+          className="btn-coral btn-lg text-sm lg:btn-outlined-green-dark lg:btn-sm">
+          Log in
+        </Button>
+      </div> */}
     </nav>
   )
 }
